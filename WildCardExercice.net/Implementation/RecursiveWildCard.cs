a12017bb7af31d3158ec31a379d1d1b7ff8c4d44
Frederic Torres <fredericaltorres@gmail.com>
2018-12-10 19:51:10 -0500

    update files for implementation

--- WildCardExercice.net/Implementation/RecursiveWildCard.cs
+++ WildCardExercice.net/Implementation/RecursiveWildCard.cs
@@ -2,15 +2,6 @@
 
 namespace WildCardExercice.net
 {
-    public class WildCardBase 
-    {
-        protected const char WILDCARD_ANY_CHAR_ZERO_OR_MORE = '*';
-        protected const char WILDCARD_ANY_CHAR_ONE_OR_MORE  = '+';
-        protected const char WILDCARD_ANY_CHAR              = '?';
-
-        protected string WILDCARD_ANY_CHAR_ZERO_OR_MORE_TWICE = WILDCARD_ANY_CHAR_ZERO_OR_MORE.ToString()+WILDCARD_ANY_CHAR_ZERO_OR_MORE.ToString();
-        protected string WILDCARD_ANY_CHAR_ONE_OR_MORE_TWICE = WILDCARD_ANY_CHAR_ONE_OR_MORE.ToString()+WILDCARD_ANY_CHAR_ONE_OR_MORE.ToString();
-    }
     /// <summary>
     /// Wildcard implementation using recursion
     /// </summary>
---
+++ WildCardExercice.net/Implementation/WildCardBase.cs
@@ -0,0 +1,12 @@
+﻿namespace WildCardExercice.net
+{
+    public class WildCardBase 
+    {
+        protected const char WILDCARD_ANY_CHAR_ZERO_OR_MORE = '*';
+        protected const char WILDCARD_ANY_CHAR_ONE_OR_MORE  = '+';
+        protected const char WILDCARD_ANY_CHAR              = '?';
+
+        protected string WILDCARD_ANY_CHAR_ZERO_OR_MORE_TWICE = WILDCARD_ANY_CHAR_ZERO_OR_MORE.ToString()+WILDCARD_ANY_CHAR_ZERO_OR_MORE.ToString();
+        protected string WILDCARD_ANY_CHAR_ONE_OR_MORE_TWICE = WILDCARD_ANY_CHAR_ONE_OR_MORE.ToString()+WILDCARD_ANY_CHAR_ONE_OR_MORE.ToString();
+    }
+}
